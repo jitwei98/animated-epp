735ad71852676b1fcdb4ee5f9d2709a3ee7f5dc1
LIGOgravitywave <tainhang3@hotmail.com>
2018-08-09 13:00:43 +0800

    Changed
    Make a main templete for head and footer in layout/lecture, so all changes to  the <link><css>for main layout goes there. Add the link to make it work. Also, all title has been changed using router

--- routes/lectures/index.js
+++ routes/lectures/index.js
@@ -14,42 +14,42 @@ router.all('/*',(req,res,next)=>{
 });
 // ohm's law
 router.get('/',(req,res)=>{
-    res.render('lecture/index');
+    res.render('lecture/index',{title:'Home Page'});
 });
 router.get('/ohms_law',(req,res)=>{
-    res.render('lecture/ohms_law');
+    res.render('lecture/ohms_law',{title:"Ohm's Law"});
 });
 //AC side
 router.get('/RLC_circuit',(req,res)=>{
     res.render('lecture/index_RLC');
 });
 router.get('/RC_circuit',(req,res)=>{
-    res.render('lecture/RC_circuit');
+    res.render('lecture/RC_circuit',{title:"RC Circuit (AC)"});
 });
 router.get('/RL_circuit',(req,res)=>{
-    res.render('lecture/RL_circuit');
+    res.render('lecture/RL_circuit',{title:"RL Circuit (AC)"});
 });
 router.get('/RLC_series_circuit',(req,res)=>{
-    res.render('lecture/RLC_series_circuit');
+    res.render('lecture/RLC_series_circuit',{title:"RLC Circuit (AC)"});
 });
 //DC side
 router.get('/RLC_circuit_DC',(req,res)=>{
-    res.render('lecture/index_RLC_DC');
+    res.render('lecture/index_RLC_DC',{title:"RLC Circuit (DC)"});
 });
 router.get('/RC_circuit_DC',(req,res)=>{
-    res.render('lecture/RC_circuit_DC');
+    res.render('lecture/RC_circuit_DC',{title:"RC Circuit (DC)"});
 });
 router.get('/RL_circuit_DC',(req,res)=>{
-    res.render('lecture/RL_circuit_DC');
+    res.render('lecture/RL_circuit_DC',{title:"RL Circuit (DC)"});
 });
 //filters
 router.get('/filter_index',(req,res)=>{
-    res.render('lecture/index_filter');
+    res.render('lecture/index_filter',);
 });
 router.get('/high_pass_filter',(req,res)=>{
-    res.render('lecture/high_pass_filter');
+    res.render('lecture/high_pass_filter',{title:"High Pass Filter"});
 });
 router.get('/low_pass_filter',(req,res)=>{
-    res.render('lecture/low_pass_filter');
+    res.render('lecture/low_pass_filter',{title:"Low Pass Filter"});
 });
 module.exports = router;
