3fe45b5e116c0b80b1df2aa64c5d207a9743550a
LIGOgravitywave <tainhang3@hotmail.com>
2018-08-12 13:15:46 +0800

    add

--- routes/admin/lecture_upload.js
+++ routes/admin/lecture_upload.js
@@ -4,8 +4,9 @@ const {userAuthenticated} = require('../../helpers/authentication');
 
 const {isEmpty, uploadDir } = require('../../helpers/upload-helper');
 const User = require('../../models/user');
-
+const fs = require('fs');
 const ohms_Law = require('../../models/lectures/ohms_law');
+const path = require('path');
 //overwrite default layout
 router.all('/*',(req,res,next)=>{
 
@@ -20,7 +21,7 @@ router.get('/ohms_law',(req,res)=>{
             ohms_Law.find({}).then(slide=>{
                 if(user.authentication.toString() == "lecturer" ){
 
-                    res.render('admin/lecture_upload/index',{UserId:UserId,topic:"ohms_law"});
+                    res.render('admin/lecture_upload/index',{UserId:UserId,topic:"ohms_law",slide:slide});
                 }else{
                     req.app.locals.layout = 'home';
                     res.render('home/index');
@@ -42,7 +43,7 @@ router.get('/ohms_law/create',(req,res)=>{
             ohms_Law.find({}).then(slide=>{
                 let slideNum = slide.length+1;
                 for(let i=0;i<slide.length;i++){
-                    if(slide.order != i+1){
+                    if(slide[i].order != i+1){
                         slideNum = i+1;
                         break;
                     }
@@ -80,18 +81,49 @@ router.post('/ohms_law/create',(req,res)=>{
        file.mv('./public/uploads/'+filename,(err)=>{
            if(err) throw err;
        })
-       const newSlide = new ohms_Law({
-           file: filename,
-           order:req.body.slideNum,
-       });
+       res.render('lecture/uploading/high_pass_filter',{files:filename,UserId:UserId,topic:'ohms_law',slideNum:req.body.slideNum});
 
-       newSlide.save().then(newSlide=>{
-           console.log('redirected');
-           res.render('lecture/uploading/high_pass_filter',{files:filename});
-       });
 
    }
 
+});
+
+router.post('/ohms_law/create/confirm',(req,res)=>{
+    var newDir = path.join(__dirname,'../../public/lecture/ohm_law/')+ req.body.files;
+    fs.rename(uploadDir+req.body.files, newDir, (err) => {
+        if (err) throw err;
+    });
+    const newSlide = new ohms_Law({
+        file: req.body.files,
+        order:req.body.slideNum,
+    });
+    newSlide.save().then(newSlide=>{
+        console.log('redirected');
+    });
+    res.render('admin/lecture_upload/create');
+
+});
+
+router.post('/ohms_law/create/cancel',(req,res)=> {
+    fs.readdir(uploadDir, (err, files) => {
+        if (err) throw err;
+
+        for (const file of files) {
+            fs.unlink(path.join(uploadDir, file), err => {
+                if (err) throw err;
+            });
+        }
+    });
+    res.render('admin/lecture_upload/create');
+
+});
+router.delete('/ohms_law/:id',(req,res)=>{
+    let UserId = UserID(req);
+    ohms_Law.remove({_id: req.params.id})
+        .then(result=>{
+            req.flash('success_message',`Slide was successfully deleted`);
+            res.redirect('/admin/lecture_upload/'+UserId + "/ohms_law");
+        });
 
 });
 function UserID(req){
